f5132b3c4ed6dc34cb2e04a94cc0887ede810521
Amit Mhetre <amit@ikigai-technology.com>
2017-09-12 20:03:40 +0530

    added basic validation for title, url and added placeholder in form fields

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ActiveRecord::Base
+  validates :title, presence: { message: "Please enter title" }
+  validates :given_url, presence: { message: "Please enter URL" }
+
   after_create :generate_short_url
   before_create :add_protocol_to_given_url
   # Generate short url after creating link object
