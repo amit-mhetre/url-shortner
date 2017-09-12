888b1206d7f2be7c8b8ab00d170aea5ac95cd952
Amit Mhetre <amit@ikigai-technology.com>
2017-09-12 19:50:38 +0530

    set root path for application and removed turbolinks from application.js to prevent double clicking

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :links
 
   get ':slug' => 'links#go_to_site'
+
+  root 'links#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
