b42be7b24ca9a8972437560c7f2f9ba4ea3cde96
Amit Mhetre <amit@ikigai-technology.com>
2017-09-12 19:39:34 +0530

    Created link model to store objects related to shorten url

---
+++ app/models/link.rb
@@ -0,0 +1,22 @@
+class Link < ActiveRecord::Base
+  after_create :generate_short_url
+  before_create :add_protocol_to_given_url
+  # Generate short url after creating link object
+  def generate_short_url
+    puts "-------> Generating short URL for link with ID : #{self.id}"
+    self.shorten_url = self.id.to_s(36)
+    self.save
+  end
+
+  def display_full_url
+    BASE_URL + self.shorten_url
+  end
+
+  def add_protocol_to_given_url
+    unless (self.given_url.start_with?('http') || self.given_url.start_with?('https'))
+      puts "--------> Prepending http protocol to given url"
+      self.given_url.prepend('http://')
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :links
+
+  get ':slug' => 'links#go_to_site'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
